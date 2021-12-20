da2f6c91bb3c4618d32ae504fb62a665aab96f25
dinushchathurya <dinushchathurya21@gmail.com>
2021-12-20 20:46:06 +0530

    feat: Add function to generate random number

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,32 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function generateCode()
+    {
+        $code = rand(1000, 9999);
+  
+        UserCode::updateOrCreate(
+            [ 'user_id' => auth()->user()->id ],
+            [ 'code' => $code ]
+        );
+  
+        $receiverNumber = auth()->user()->phone;
+        $message = "2FA login code is ". $code;
+    
+        try {
+   
+            $account_sid = getenv("TWILIO_SID");
+            $auth_token = getenv("TWILIO_TOKEN");
+            $twilio_number = getenv("TWILIO_FROM");
+    
+            $client = new Client($account_sid, $auth_token);
+            $client->messages->create($receiverNumber, [
+                'from' => $twilio_number, 
+                'body' => $message]);
+    
+        } catch (Exception $e) {
+            info("Error: ". $e->getMessage());
+        }
+    }
 }
